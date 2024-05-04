286004bcafde65ab65333f013df09c88992a5baf
shreyas-pedda <spedda@umass.edu>
2024-05-04 17:12:17 -0400

    Ultrasound sensors are working, along with car controls, and joystick input. Need to accurately created joystick object to send over and also implement PWM.

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,16 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-#define LEFT_WIRE 12 // White
-#define RIGHT_WIRE 13 // Purple
-#define UP_WIRE 27 // Brown
-#define DOWN_WIRE 14 // Yellow
-#define TRIG1 2
-#define ECHO1 15
-#define TRIG2 33
-#define ECHO2 25
-//#define TRIG3 
-//#define ECHO3 
+#define LEFT_WIRE 13 // Dark Blue - 1
+#define RIGHT_WIRE 12 // Turquoise - 2
+#define UP_WIRE 14 // Red - 3, yellow  25
+#define DOWN_WIRE 27 // Black - 4, black
+#define TRIG1 15
+#define ECHO1 2
+#define TRIG2 16
+#define ECHO2 4
+#define TRIG3 17
+#define ECHO3 5
 typedef struct test_struct {
   int x;
   int y;
@@ -52,6 +52,8 @@ void setup(){
   pinMode(RIGHT_WIRE, OUTPUT);
   pinMode(UP_WIRE, OUTPUT);
   pinMode(DOWN_WIRE, OUTPUT);
+  Serial.println(WiFi.macAddress());
+
   
   pinMode(TRIG1, OUTPUT); // set up distance1 sensor pins
   pinMode(ECHO1, INPUT);
@@ -59,15 +61,13 @@ void setup(){
   pinMode(TRIG2, OUTPUT); // set up distance2 sensor pins
   pinMode(ECHO2, INPUT);
   digitalWrite(TRIG2, LOW);
-//  pinMode(TRIG3, OUTPUT); // set up distance3 sensor pins
-//  pinMode(ECHO3, INPUT);
-//  digitalWrite(TRIG3, LOW);
+  pinMode(TRIG3, OUTPUT); // set up distance3 sensor pins
+  pinMode(ECHO3, INPUT);
+  digitalWrite(TRIG3, LOW);
 
-  ledcSetup(0, pwmFrequency, pwmResolution);
-  ledcAttachPin(UP_WIRE, 0);
+  // ledcSetup(0, pwmFrequency, pwmResolution);
+  // ledcAttachPin(UP_WIRE, 0);
 }
-
-#define QUEUE_SIZE 5
 
 // read distance sensor, return centimeters
 float readDistance(int TRIG, int ECHO) {
@@ -91,21 +91,26 @@ float averageDistance(int TRIG, int ECHO){
   }
   return totalLapse / 5;
 }
-// A8:42:E3:BA:E7:F8
 
 
 void loop(){
   float dist1 = averageDistance(TRIG1, ECHO1);
   delay(50);
   float dist2 = averageDistance(TRIG2, ECHO2);
-  // Serial.print("1: ");
-  // Serial.println(dist1);
-  // Serial.print("2: ");
-  // Serial.println(dist2);
-  ledcWrite(0, 50);
-  if (dist1 < 20 || dist2 < 20){
-    ledcWrite(0, 0);
-  }
+  delay(50);
+  float dist3 = averageDistance(TRIG3, ECHO3);
+
+  Serial.print("1: ");
+  Serial.println(dist1);
+  Serial.print("2: ");
+  Serial.println(dist2);
+  Serial.print("3: ");
+  Serial.println(dist3);
+  
+  // digitalWrite(UP_WIRE, HIGH);
+  // if (dist1 < 20 || dist2 < 20 || dist3 < 20){
+  //   digitalWrite(UP_WIRE, LOW);
+  // }
 
 //  digitalWrite(LEFT_WIRE, HIGH);
 //  delay(1000);
@@ -114,6 +119,7 @@ void loop(){
 //  delay(1000);
 //  digitalWrite(RIGHT_WIRE, LOW);
 //  delay(5000);
+
  
 //  digitalWrite(DOWN_WIRE, HIGH);
 //  delay(1000);
@@ -124,5 +130,6 @@ void loop(){
 //  delay(1000);
 //  digitalWrite(UP_WIRE, LOW);
 //  delay(2000); 
-  
+
+
 }
